d1065a9ac18c4a83fb3caf747a32e947235186e1
Darrow8 <darrowrhartman@gmail.com>
2025-08-10 12:19:29 -0700

    gpt-oss code from modal example repo

--- run.py
+++ run.py
@@ -1,4 +1,23 @@
 import modal
+import json
+from typing import Any
+import aiohttp
+import modal
+
+vllm_image = (
+    modal.Image.debian_slim(python_version="3.12")
+    .pip_install(
+        "vllm==0.9.1",
+        "huggingface_hub[hf_transfer]==0.32.0",
+        "flashinfer-python==0.2.6.post1",
+        extra_index_url="https://download.pytorch.org/whl/cu128",
+    )
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})  # faster model transfers
+)
+MODEL_NAME = "RedHatAI/Meta-Llama-3.1-8B-Instruct-FP8"
+MODEL_REVISION = "12fd6884d2585dd4d020373e7f39f74507b31866"  # avoid nasty surprises when repos update!
+hf_cache_vol = modal.Volume.from_name("huggingface-cache", create_if_missing=True)
+vllm_cache_vol = modal.Volume.from_name("vllm-cache", create_if_missing=True)
 
 app = modal.App("example-get-started")
 
